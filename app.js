aaaaf540e1bd365879b2a701993037257f4c2927
saliyomotana <115626612+saliyomotana@users.noreply.github.com>
2022-11-20 12:53:57 +0000

    done
    went through code and fixed bugs and wrong code.

--- app.js
+++ app.js
@@ -1,54 +1,64 @@
 const choices = ["Rock", "Paper", "Scissors"];
-const playerSelection = window.prompt("Enter your choice:").toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber]
 }
 
-function playRound(playerSelection, computerSelection) {
 
-        if (playerSelection === "Rock") {
+const playerChoices = document.querySelectorAll('.options')
+for (let i = 0; i < playerChoices.length; i++) {
+    playerChoices[i].addEventListener("click", (e) => {
+        playRound(e.target.value, getComputerChoice())
+        //console.log(e.target.value, getComputerChoice())
+    });
+}
+
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection, computerSelection)
+    if (computerSelection === playerSelection) {
+        return "Tie"
+    }
+    switch (playerSelection) {
+        case "Rock":
             if (computerSelection === "Paper") {
-                return "You Lose!";
-        } else if (computerSelection === "Scissors") {
-                return "You Win!";
-        } else {
-                return "It is a tie";
-        }
-        } else if (playerSelection === "Paper") {
+                console.log("You lose!")
+            } else {
+                console.log("You Win")
+            }
+            break;
+        case "Paper":
             if (computerSelection === "Scissors") {
-                return "You Lose!";
-        } else if (computerSelection === "Rock") {
-                return "You Win!";
-        } else {
-                return "It is a tie";
-        }
-        } else if (playerSelection === "Scissors") {
+                console.log("You lose!")
+            } else {
+                console.log("You Win")
+            }
+            break;
+        case "Scissors":
             if (computerSelection === "Rock") {
-            return "You Lose!";
-        } else if (computerSelection === "Paper") {
-                return "You Win!";
-        } else {
-                return "It is a tie";
-        }
-        }
+                console.log("You lose!")
+            } else {
+                console.log("You Win")
+            }
+            break;
+        default:
+            console.log("player weapon not found")
+            break;
+    }
 }
 
 function game(playRound) {
     for (let i = 0; i < 5; i++) {
-        if (i = 1) {
+        if (i === 1) {
             return "You have 1 point";
         } 
-        else if (i = 2) {
+        else if (i === 2) {
             return "You have 2 points";
         }
-        else if (i = 3) {
+        else if (i === 3) {
             return "You have 3 points";
         }
-        else if (i = 4) {
+        else if (i === 4) {
             return "You have 4 points";
         }
         else {
